1f693cf49eae791bc2d5458c9c9aacdf653c2a1c
Raimon Redondo <raimon.redondo@rarejava.com>
2015-11-04 12:26:47 +0100

    Refactor SequencingActorTest to use JavaTestKit as in Akka documentation

--- basics/src/test/java/akka/training/basics/actor/SequencingActorTest.java
+++ basics/src/test/java/akka/training/basics/actor/SequencingActorTest.java
@@ -4,51 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.typesafe.config.ConfigFactory;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.testkit.TestKit;
+import akka.testkit.JavaTestKit;
 
-public class SequencingActorTest extends TestKit {
+public class SequencingActorTest {
 
-    static ActorSystem system = ActorSystem.create("actor-system-test", ConfigFactory.load());
+    static ActorSystem system;
 
-    public SequencingActorTest() {
-        super(system);
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
     }
 
     @Test
     public void testSequencingActor() {
-        final List<Integer> headList = new ArrayList<Integer>();
-        final List<Integer> tailList = new ArrayList<Integer>();
+        new JavaTestKit(system) {
 
-        final int randomHead = new Random().nextInt(6);
-        final int randomTail = new Random().nextInt(10);
+            {
 
-        for (int i = 0; i < randomHead; i++) {
-            headList.add(i);
-        }
-        for (int i = 1; i < randomTail; i++) {
-            tailList.add(i);
-        }
+                final List<Integer> headList = new ArrayList<Integer>();
+                final List<Integer> tailList = new ArrayList<Integer>();
 
-        final ActorRef sequencingActorRef = system.actorOf(Props.create(SequencingActor.class, super.testActor(),
-                headList, tailList));
-        // pass the reference to implicit sender testActor() otherwise
-        // message end up in dead mailbox
-        sequencingActorRef.tell("do something", super.testActor());
+                final int randomHead = new Random().nextInt(6);
+                final int randomTail = new Random().nextInt(10);
 
-        for (final Integer value : headList) {
-            expectMsgClass(Integer.class);
-        }
-        expectMsg("do something");
-        for (final Integer value : tailList) {
-            expectMsgClass(Integer.class);
-        }
-        expectNoMsg();
+                for (int i = 0; i < randomHead; i++) {
+                    headList.add(i);
+                }
+                for (int i = 1; i < randomTail; i++) {
+                    tailList.add(i);
+                }
+
+                final Props props = Props.create(SequencingActor.class, headList, tailList);
+                final ActorRef subject = system.actorOf(props);
+                // pass the reference to implicit sender testActor() otherwise
+                // message end up in dead mailbox
+                subject.tell("do something", getRef());
+
+                for (final Integer value : headList) {
+                    expectMsgClass(Integer.class);
+                }
+                expectMsgEquals("do something");
+                for (final Integer value : tailList) {
+                    expectMsgClass(Integer.class);
+                }
+                expectNoMsg();
+
+            }
+        };
+
     }
+
 }
