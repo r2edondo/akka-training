05506e0e0604e3d7736aa240c9f995e7257c7c2e
Raimon Redondo <raimon.redondo@rarejava.com>
2015-11-04 12:18:28 +0100

    Refactor ForwardActorTest to use JavaTestKit as in Akka documentation

--- basics/src/test/java/akka/training/basics/actor/ForwardActorTest.java
+++ basics/src/test/java/akka/training/basics/actor/ForwardActorTest.java
@@ -1,26 +1,40 @@
 package akka.training.basics.actor;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.typesafe.config.ConfigFactory;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.testkit.TestKit;
+import akka.testkit.JavaTestKit;
 
-public class ForwardActorTest extends TestKit {
+public class ForwardActorTest {
 
-    static ActorSystem system = ActorSystem.create("actor-system-test", ConfigFactory.load());
+    static ActorSystem system;
 
-    public ForwardActorTest() {
-        super(system);
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
     }
 
     @Test
     public void testForwardingActorForwardsTheMessageSuccessfully() {
-        final ActorRef forwardingActorRef = system.actorOf(Props.create(ForwadingActor.class, super.testActor()));
-        forwardingActorRef.tell("testActor should recieve this message",null);
-        expectMsg("testActor should recieve this message");
+        new JavaTestKit(system) {
+
+            {
+                final Props props = Props.create(ForwadingActor.class, getRef());
+                final ActorRef subject = system.actorOf(props);
+                subject.tell("testActor should recieve this message", null);
+                expectMsgEquals("testActor should recieve this message");
+            }
+        };
+
     }
 }
